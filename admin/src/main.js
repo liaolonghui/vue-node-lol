e36c57c8d15de130ef49664add17f6e1d1db84f7
liaolonghui <1340881930@qq.com>
2021-02-22 15:26:50 +0800

    后台管理使用element上传图片时携带token

--- admin/src/main.js
+++ admin/src/main.js
@@ -12,6 +12,23 @@ Vue.config.productionTip = false
 import http from './http'
 Vue.prototype.$http = http
 
+// 使用混入，此处用于给<el-upload>提供headers（Authorization）
+// 混入的是一个方法，这样可以取到实时的Authorization
+Vue.mixin({
+  methods: {
+    getAuthHeaders() {
+      return {
+        Authorization: `Bearer ${localStorage.token || ''}`
+      }
+    }
+  },
+  computed: {
+    uploadUrl() {
+      return http.defaults.baseURL + 'upload'
+    }
+  }
+})
+
 new Vue({
   router,
   render: h => h(App)
--- admin/src/views/AdEdit.vue
+++ admin/src/views/AdEdit.vue
@@ -15,7 +15,8 @@
             <el-form-item label="图片">
               <el-upload
                 class="avatar-uploader"
-                :action="$http.defaults.baseURL + 'upload'"
+                :action="uploadUrl"
+                :headers="getAuthHeaders()"
                 :show-file-list="false"
                 :on-success="res => $set(item, 'image', res.url)">
                 <img v-if="item.image" :src="item.image" class="avatar">
