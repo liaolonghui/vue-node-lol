a3b12d59b08f04b16fc4acda9faeeeb6ee31e3ab
liaolonghui <1340881930@qq.com>
2021-03-02 09:46:17 +0800

    完成移动端英雄详情页的一部分

--- web/src/views/Hero.vue
+++ web/src/views/Hero.vue
@@ -10,9 +10,25 @@
       <router-link tag="span" to="/">更多英雄 &gt;</router-link>
     </div>
     <!-- 具体详情 -->
-    <div>{{hero.name}}</div>
+    <div class="top" :style="{'background-image': `url(${hero.banner})`}">
+      <div class="info pl-2 pb-2 text-white h-100 d-flex flex-column jc-end">
+        <span class="text-light-1 fs-md">{{hero.title}}</span>
+        <span class="fs-xl py-2">{{hero.name}}</span>
+        <span class="text-light-1 fs-md">{{hero.categories.map(v => v.name).join('/')}}</span>
+        <div class="scores pt-1 d-flex ai-center" v-if="hero.scores">
+          <span>难度</span>
+          <span class="dotted bg-primary">{{hero.scores.difficult}}</span>
+          <span>技能</span>
+          <span class="dotted bg-blue">{{hero.scores.skills}}</span>
+          <span>攻击</span>
+          <span class="dotted bg-red">{{hero.scores.attack}}</span>
+          <span>生存</span>
+          <span class="dotted bg-green">{{hero.scores.survive}}</span>
+        </div>
+      </div>
+    </div>
   </div>
-  <div v-else class="d-flex jc-center ai-center">
+  <div v-else class="d-flex jc-center ai-center h-100 no-hero">
     <h2>英雄不存在</h2>
     <router-link tag="div" to="/" class="bg-primary text-white text-center ml-3" style="width: 50px; height: 25px; line-height: 25px; border-radius: 25px;">首页</router-link>
   </div>
@@ -39,3 +55,35 @@ export default {
   },
 }
 </script>
+
+<style lang="scss">
+.page-hero {
+  .top {
+    height: 50vw;
+    background: #fff no-repeat top center;
+    background-size: 100% auto;
+    .info {
+      background: linear-gradient(rgba(0,0,0,0), rgba(0,0,0,0.9));
+      .scores {
+        .dotted {
+          display: inline-block;
+          width: 1rem;
+          height: 1rem;
+          line-height: 1rem;
+          font-size: 0.8rem;
+          text-align: center;
+          border-radius: 50%;
+          border: 0.0385rem solid #fff;
+          margin: 0 0.25rem;
+        }
+      }
+    }
+  }
+}
+
+.no-hero {
+  background: url(../assets/images/bg.jpg) no-repeat 0 0;
+  background-size: cover;
+  padding-top: 23.0769rem;
+}
+</style>
