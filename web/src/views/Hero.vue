4ef3e0be19bb0e6f41612ee2790662f5f389ec16
liaolonghui <1340881930@qq.com>
2021-03-02 11:36:42 +0800

    移动端的英雄详情页的技能详情展示

--- web/src/views/Hero.vue
+++ web/src/views/Hero.vue
@@ -27,10 +27,55 @@
         </div>
       </div>
     </div>
-  </div>
-  <div v-else class="d-flex jc-center ai-center h-100 no-hero">
-    <h2>英雄不存在</h2>
-    <router-link tag="div" to="/" class="bg-primary text-white text-center ml-3" style="width: 50px; height: 25px; line-height: 25px; border-radius: 25px;">首页</router-link>
+    <!-- end of top -->
+    <div>
+      <div class="bg-white px-3 pt-1">
+        <div class="nav d-flex jc-around py-2 border-bottom">
+          <div class="nav-item" :class="{active: active === 0}" @click="$refs.mySwiper.$swiper.slideTo(0)">
+            <div class="nav-link">英雄详情</div>
+          </div>
+          <div class="nav-item" :class="{active: active === 1}" @click="$refs.mySwiper.$swiper.slideTo(1)">
+            <div class="nav-link">更多信息</div>
+          </div>
+        </div>
+      </div>
+      <!-- swiper -->
+      <swiper ref="mySwiper" @slide-change="() => active=$refs.mySwiper.$swiper.realIndex">
+        <swiper-slide>
+          <div>
+            <div class="bg-white pt-2 pb-3 border-bottom">
+              <!-- router -->
+              <div class="d-flex jc-around px-3">
+                <router-link to="/about" tag="button" class="flex-1 btn btn-lg fs-md">
+                  <i class="iconfont icon-LOLyuanxing text-primary pr-2"></i>
+                  <span>更多英雄</span>
+                </router-link>
+                <router-link to="/about" tag="button" class="flex-1 btn btn-lg ml-2">
+                  <i class="iconfont icon-menu text-primary pr-2"></i>
+                  <span>了解装备</span>
+                </router-link>
+              </div>
+              <!-- skills -->
+              <!-- skills不为空数组时展示 -->
+              <div class="skills mt-4" v-if="hero.skills.length">
+                <div class="d-flex jc-around">
+                  <img :src="item.icon" v-for="(item, i) in hero.skills" :key="i" @click="currentIndex = i" class="icon" :class="{active: currentIndex === i}">
+                </div>
+                <div class="px-3">
+                  <h3>{{currentSkill.name}}</h3>
+                  <p>{{currentSkill.description}}</p>
+                  <div class="border-bottom"></div>
+                  <p>小提示：{{currentSkill.tips}}</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </swiper-slide>
+        <swiper-slide>
+          <div><h1>NO MORE</h1></div>
+        </swiper-slide>
+      </swiper>
+    </div>
   </div>
 </template>
 
@@ -41,7 +86,14 @@ export default {
   },
   data() {
     return {
-      hero: null
+      active: 0,
+      hero: null,
+      currentIndex: 0
+    }
+  },
+  computed: {
+    currentSkill() {
+      return this.hero.skills[this.currentIndex]
     }
   },
   methods: {
@@ -57,6 +109,8 @@ export default {
 </script>
 
 <style lang="scss">
+@import '../assets/scss/variables.scss';
+
 .page-hero {
   .top {
     height: 50vw;
@@ -81,9 +135,17 @@ export default {
   }
 }
 
-.no-hero {
-  background: url(../assets/images/bg.jpg) no-repeat 0 0;
-  background-size: cover;
-  padding-top: 23.0769rem;
+// skillImgs
+.skills {
+  img.icon {
+    width: 60px;
+    height: 60px;
+    border-radius: 50%;
+    border: 4px solid map-get($map: $colors, $key: 'white');
+    &.active {
+      border-color: map-get($map: $colors, $key: 'primary');
+    }
+  }
 }
+
 </style>
